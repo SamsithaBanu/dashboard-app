c4d8247d2ca63e4aa80b31a94ff45ebacf521a2f
clouding-land <samsitha.s@asterdmhealthcare.com>
2025-02-04 14:45:30 +0530

    added middle and bottom components

--- src/styles/HeaderStyled.js
+++ src/styles/HeaderStyled.js
@@ -7,8 +7,8 @@ export const HeaderStyled = styled.div`
   align-items: center;
   background: #4f45e4;
   padding: 15px 20px;
-  width:93%;
-  position: fixed;
+  // width:93%;
+  // position: fixed;
   .leftWrapper {
     display: flex;
     flex-direction: row;
